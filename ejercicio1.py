10612e5044d0633fb1c2ae4c4b47e02a8506c7f8
Brianmuvel <brianmuvel@gmail.com>
2021-02-20 12:15:31 -0600

    funcionSImplificada

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,13 +1,27 @@
 
+# def largo_numero(n):
+#     contador = 0
+#     if  isinstance(n, int):
+#         if n < -1:
+#             print('El valor tiene que ser positivo')
+#         elif n >=0:
+#             for i in str(n):
+#                 contador += 1
+#             print(f'El largo del número es de: {contador} caracteres')
+#     elif isinstance(n, str):
+#         print('El valor tiene que ser integral')
+# largo_numero(1234)
+
+
 def largo_numero(n):
-    contador = 0
-    if  isinstance(n, int):
-        if n < -1:
-            print('El valor tiene que ser positivo')
-        elif n >=0:
-            for i in str(n):
-                contador += 1
-            print(f'El largo del número es de: {contador} caracteres')
-    elif isinstance(n, str):
+    contador=0
+    if isinstance(n, str):
         print('El valor tiene que ser integral')
-largo_numero(1234)
+    elif n < -1:
+        print('El valor tiene que ser positivo')  
+    else:
+        for i in str(n):
+            contador += 1
+        print(f'El largo del número es de: {contador}')
+
+largo_numero('asd')
